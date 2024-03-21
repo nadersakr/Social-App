1cb0f41174f4d0836398fc696bc7591c82f851d5
nadersakr <nader150656@eng.psu.edu.eg>
2024-03-21 05:01:04 +0200

    Add imports and modify HomeScreen widget

--- social_app/lib/views/screens/Home/home.dart
+++ social_app/lib/views/screens/Home/home.dart
@@ -1,5 +1,7 @@
+import 'package:enefty_icons/enefty_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:social_app/utils/colors.dart';
 import 'package:social_app/views/screens/Home/home_main_contents.dart';
 import 'package:social_app/views/screens/auth/widgets/home_drawer.dart';
 import 'package:social_app/provider/auth/auth.dart';
@@ -12,7 +14,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   @override
   Widget build(BuildContext context) {
     print("homeeeeeeeee rebuild");
@@ -20,6 +21,7 @@ class _HomeScreenState extends State<HomeScreen> {
         Provider.of<AuthController>(context, listen: false);
     return SafeArea(
       child: Scaffold(
+       
         body: SafeArea(
           child: FutureBuilder(
             future: authControllerListenFalse.getAppData(),
@@ -36,9 +38,58 @@ class _HomeScreenState extends State<HomeScreen> {
             },
           ),
         ),
-        
         drawer: HomeDrawer(authController: authControllerListenFalse),
       ),
     );
   }
 }
+
+// class HomeNavigationBar extends StatelessWidget {
+//   const HomeNavigationBar({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return BottomNavigationBar(
+//       showSelectedLabels: false,
+//       showUnselectedLabels: false,
+//       fixedColor: Colors.white,
+//       items: const [
+//         BottomNavigationBarItem(
+//           icon: Icon(
+//             EneftyIcons.home_outline,
+//             color: AppColors.darkBlack,
+//           ),
+//           label: 'h',
+//         ),
+//         BottomNavigationBarItem(
+//           icon: Icon(
+//             EneftyIcons.message_2_outline,
+//             color: AppColors.darkBlack,
+//           ),
+//           label: 'm',
+//         ),
+//         BottomNavigationBarItem(
+//           icon: Icon(
+//             EneftyIcons.add_circle_outline,
+//             color: AppColors.darkBlack,
+//           ),
+//           label: 'a',
+//         ),
+//         BottomNavigationBarItem(
+//           icon: Icon(
+//             EneftyIcons.heart_outline,
+//             color: AppColors.darkBlack,
+//           ),
+//           label: 'n',
+//         ),
+//         BottomNavigationBarItem(
+//           icon: Icon(
+//             EneftyIcons.user_outline,
+//             color: AppColors.darkBlack,
+//           ),
+//           label: 'p',
+//         ),
+//       ],
+//     );
+//   }
+// }
