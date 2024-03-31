43b5aab4e94c5488f8a935d6c957e3274a015cb4
nadersakr <nader150656@eng.psu.edu.eg>
2024-03-31 05:58:47 +0200

    Add FavoriteScreen widget

---
+++ social_app/lib/views/screens/favouirte_posts_screen/favourate_posts.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class FavoriteScreen extends StatelessWidget {
+  const FavoriteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              expandedHeight: 200.0,
+              title: const Text('nader Header'),
+              floating: true,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                title: const Text('Collapsible Header'),
+                background: Image.network(
+                  'https://via.placeholder.com/500x200',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text('Item $index'),
+                  );
+                },
+                childCount: 50,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
