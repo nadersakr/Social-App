cde4d948de78542ef83953276e3d9158881ae65d
nadersakr <nader150656@eng.psu.edu.eg>
2024-03-31 05:58:12 +0200

    Add darkBlue color constant

--- social_app/lib/utils/colors.dart
+++ social_app/lib/utils/colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color lightGray = Color(0xffF3F5F7);
   static const Color blue = Color(0xffE1F6F4);
+  static const Color darkBlue = Color(0xff7DB9B3);
   static const Color white = Colors.white;
   static const Color buttonGradientOne = Color(0xffFFE1E0);
   static const Color buttonGradientTwo = Color(0xffE1F6F4);
