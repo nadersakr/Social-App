7e126f080cf6332abeb6a7ad8f1a8e4d3510be0b
nadersakr <nader150656@eng.psu.edu.eg>
2024-04-01 16:49:38 +0200

    Refactor image upload and add post document

--- social_app/lib/views/screens/Home/add_post.dart
+++ social_app/lib/views/screens/Home/add_post.dart
@@ -145,40 +145,44 @@ class AddPostScreenState extends State<AddPostScreen> {
                           ),
                         );
                         var storage = FirebaseFirestore.instance;
-                        var posts = storage.collection("posts");
+                        var user = storage
+                            .collection("users")
+                            .doc(authController.mainUser.userUID);
 
                         // Upload image to Firebase Storage
                         String? imageUrl;
-                        if (_image != null) {
-                          try {
-                            // Create a reference to the file location in Firebase Storage
-                            Reference ref =
-                                FirebaseStorage.instance.ref().child(
-                                      'posts/${path.basename(_image!.path)}',
-                                    );
 
-                            // Upload the file to Firebase Storage
-                            await ref.putFile(_image!);
+                        try {
+                          // Create a reference to the file location in Firebase Storage
+                          Reference ref = FirebaseStorage.instance.ref().child(
+                                'posts/${path.basename(_image!.path)}',
+                              );
 
-                            // Get the download URL
-                            imageUrl = await ref.getDownloadURL();
-                            print(imageUrl);
-                          } catch (e) {
-                            print(e);
-                            // Handle any errors
-                          }
+                          // Upload the file to Firebase Storage
+                          await ref.putFile(_image!);
+
+                          // Get the download URL
+                          imageUrl = await ref.getDownloadURL();
+                        } catch (e) {
+                          FocusManager.instance.primaryFocus?.unfocus();
+                          Navigator.of(context).pop();
+                          SnackBar snackBar = const SnackBar(
+                            content: Text('Failed to upload image'),
+                          );
+
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
                         }
 
-                        // Add the post document with the image URL
-                        await posts.add({
+                        Map<String, String> post = {
                           'time':
                               '${hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')} $amPm, ${now.day.toString().padLeft(2, '0')}/${now.month.toString().padLeft(2, '0')}/${now.year.toString().padLeft(2, '0')}',
-
-                          'owner': authController.mainUser.userUID,
-                          'imageUrl':
-                              imageUrl, // Add the image URL to the document
+                          'imageUrl': imageUrl ??
+                              "https://firebasestorage.googleapis.com/v0/b/social-app-89a41.appspot.com/o/path%2Fto%2Fstorage%2F1000003515.jpg?alt=media&token=829cf6ad-5c7c-4dc1-84a7-6cdbd18e4c80", // Add the image URL to the document
+                        };
+                        // Add the post document with the image URL
+                        await user.update({
+                          'posts': FieldValue.arrayUnion([post]),
                         });
-
                         FocusManager.instance.primaryFocus?.unfocus();
                         Navigator.of(context).pop();
                       }
