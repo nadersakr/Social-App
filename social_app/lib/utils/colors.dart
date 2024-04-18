1cb8babe32dd646518b56bd20762255f36b97467
nadersakr <nader150656@eng.psu.edu.eg>
2024-04-18 19:34:36 +0200

    Update colors.dart with new color constants

--- social_app/lib/utils/colors.dart
+++ social_app/lib/utils/colors.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color lightGray = Color(0xffF3F5F7);
+  static const Color vertLightGrey = Color.fromARGB(192, 250, 250, 250);
+  static const Color midLightGrey = Color(0xffFAFAFA);
+  static const Color grey = Colors.grey;
+  static const Color midGrey = Color(0xff656565);
+  static const Color darkgrey = Color(0xff4E4E4E);
   static const Color blue = Color(0xffE1F6F4);
   static const Color darkBlue = Color(0xff7DB9B3);
   static const Color white = Colors.white;
   static const Color buttonGradientOne = Color(0xffFFE1E0);
   static const Color buttonGradientTwo = Color(0xffE1F6F4);
   static const Color red = Color.fromARGB(255, 218, 57, 94);
-  static const Color gray = Colors.grey;
-  static const Color darkgrey = Color(0xff4E4E4E);
-  static const Color midGrey = Color(0xff656565);
   static const Color black = Colors.black38;
   static const Color midBlack = Color(0xff424242);
   static const Color darkBlack = Colors.black;
