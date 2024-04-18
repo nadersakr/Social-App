95df49d7c1690136686171bdc1ff466dd1636a0b
nadersakr <nader150656@eng.psu.edu.eg>
2024-04-19 00:33:18 +0200

    Update chat UI and functionality, add new SVG assets, refactor CustomTextField widget, update authentication screen, update colors, add variables to AuthController, add backgroundChats.svg, add launch.json configuration, fix avatar image loading, add posts to user and posts collection

--- social_app/lib/views/screens/chat/chat_kit.dart
+++ social_app/lib/views/screens/chat/chat_kit.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart';
 import 'package:social_app/model/user_model.dart';
+import 'package:social_app/utils/colors.dart';
+import 'package:social_app/views/screens/Home/reusable_widgets.dart';
 import 'package:social_app/views/screens/chat/chat_screen.dart';
 
 class ChatKit extends StatelessWidget {
@@ -15,68 +19,66 @@ class ChatKit extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String formattedTime = DateFormat('jm').format(DateTime.parse(time));
-    return Column(
-      children: [
-        GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => ChatScreen(resevier: friend),
-              ),
-            );
-          },
-          child: ListTile(
-            leading: CircleAvatar(
-              radius: 30,
-              backgroundImage: NetworkImage(friend.avatar),
-            ),
-            title: Text(
-              friend.userName ?? 'User Name',
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
-              ),
-            ),
-            subtitle: Row(
-              children: <Widget>[
-                Text(
-                  lastMessage,
-                  style: const TextStyle(
-                    color: Colors.grey,
-                    fontSize: 12,
+    // String formattedTime = DateFormat('jm').format(DateTime.parse(time));
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ChatScreen(resevier: friend),
+          ),
+        );
+      },
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 8.h, horizontal: 24.w),
+        padding: EdgeInsets.all(16.w),
+        decoration: BoxDecoration(
+          color: AppColors.white.withOpacity(0.6),
+          borderRadius: BorderRadius.circular(38.r),
+        ),
+        // height: 90.h,
+        width: 1.sw - 48.w,
+        child: Row(
+          children: [
+            Container(
+                padding: EdgeInsets.all(3.w),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: AppColors.darkBlack,
+                    width: 2.w,
                   ),
                 ),
-                const Text(
-                  ' - ',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 12,
-                  ),
+                child: CircleAvatar(
+                  radius: 34.r,
+                  backgroundImage: NetworkImage(friend.avatar),
+                )),
+            SizedBox(
+              width: 21.w,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextClass(
+                  text:
+                      "${(friend.userName ?? 'User Name').substring(0, 1).toUpperCase()}${(friend.userName ?? 'User Name').substring(1)}",
+                  color: AppColors.darkBlack,
                 ),
-                Text(
-                  formattedTime, // You need to provide this variable
-                  style: const TextStyle(
-                    color: Colors.grey,
-                    fontSize: 12,
-                  ),
+                SizedBox(
+                  height: 2.h,
+                ),
+                TextClass(
+                  text: lastMessage,
+                ),
+                SizedBox(
+                  height: 24.h,
                 ),
               ],
-            ),
-            trailing: const Icon(
-              Icons.chat,
-              color: Colors.blue,
-            ),
-          ),
+            )
+          ],
         ),
-        const Divider(
-          height: 10,
-          thickness: 1,
-          indent: 20,
-          endIndent: 20,
-        ),
-      ],
+      ),
     );
   }
 }
