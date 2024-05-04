af0670522ea0a6a6cdc98418c75545b568741bb5
nadersakr <nader150656@eng.psu.edu.eg>
2024-05-05 00:36:09 +0300

    feat: Update home_main_contents.dart to load user data in FutureBuilder

--- social_app/lib/views/screens/Home/home_main_contents.dart
+++ social_app/lib/views/screens/Home/home_main_contents.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:social_app/view_model/user_viewmodel.dart';
 import 'package:social_app/views/screens/Home/home_header.dart';
 import 'package:social_app/views/screens/Home/home_story_card.dart';
 
 Widget homeMainContent(BuildContext context) {
+  UserViewModel userViewModel = UserViewModel();
   return SingleChildScrollView(
     child: Stack(
       children: [
@@ -14,7 +16,19 @@ Widget homeMainContent(BuildContext context) {
             homeHeader(context),
             homeStoryCards(context),
             const Divider(),
-
+            FutureBuilder(
+            future: userViewModel.loadingMyUserDataToUserModel(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasError) {
+                return Center(child: Text("${snapshot.error}"));
+              } else {
+                return Text("${snapshot.data!.mail}");
+              }
+            })
             // const PostsFutureBuilder(),
             // Add more post cards here as needed
           ],
