e23314f51e060dddaad6755bb1393d2f94cadad6
nadersakr <nader150656@eng.psu.edu.eg>
2024-05-04 23:06:44 +0300

    refactor: Remove unused code in home_main_contents.dart

--- social_app/lib/views/screens/Home/home_main_contents.dart
+++ social_app/lib/views/screens/Home/home_main_contents.dart
@@ -3,9 +3,6 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:social_app/views/screens/Home/home_header.dart';
 import 'package:social_app/views/screens/Home/home_story_card.dart';
 
-
-import 'package:social_app/views/screens/Home/reusable_widgets.dart';
-
 Widget homeMainContent(BuildContext context) {
   return SingleChildScrollView(
     child: Stack(
@@ -17,9 +14,8 @@ Widget homeMainContent(BuildContext context) {
             homeHeader(context),
             homeStoryCards(context),
             const Divider(),
-            // homeTapButtons(),
-            // const Divider(),
-            const PostsFutureBuilder(),
+
+            // const PostsFutureBuilder(),
             // Add more post cards here as needed
           ],
         ),
