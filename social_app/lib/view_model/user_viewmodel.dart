c133fb4a38799e75d716841fc504072548a716cf
nadersakr <nader150656@eng.psu.edu.eg>
2024-05-04 22:59:30 +0300

    refactor: Update UserViewModel to load user data based on login state and UID

--- social_app/lib/view_model/user_viewmodel.dart
+++ social_app/lib/view_model/user_viewmodel.dart
@@ -3,34 +3,31 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:social_app/model/user_model_.dart';
 import 'package:social_app/services/Firebase/Firebase%20FireStore/load_user_data.dart';
+import 'package:social_app/utils/shared-preferences/shared_preferences.dart';
 
 class UserViewModel extends ChangeNotifier {
   static UserCredential? userCredintial;
   UserModel? userModel;
-  Future<bool> loadingUser() async {
-    if (userCredintial != null) {
-      return true;
-    } else {
-      return false;
-    }
-  }
 
-  Future<bool> loadingMyUserData() async {
-    print("*********************************");
-    print(userCredintial!.user!.email);
-    if (userModel != null) {
-      return true;
-    }
-    try {
-      userModel = await FirebasefirestoreLoadUserData()
-          .loadAUserData(userCredintial!.user!.uid);
-      if (userModel != null) {
-        return true;
-      } else {
-        return false;
+  Future<UserModel?> loadingMyUserDataToUserModel() async {
+    // if login in the first time
+    if (userCredintial != null) {
+      try {
+        return await FirebasefirestoreLoadUserData()
+            .loadAUserData(userCredintial!.user!.uid);
+      } catch (e) {
+        return null;
       }
-    } catch (e) {
-      return false;
     }
+    // if the user open the app without login again
+    String? key = AppSharedPreferences.getValueString(value: "uid");
+    if (key != null) {
+      try {
+        return await FirebasefirestoreLoadUserData().loadAUserData(key);
+      } catch (e) {
+        return null;
+      }
+    }
+    return null;
   }
 }
