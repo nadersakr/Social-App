aa18d90ffb63dcfe1b960d098883d439bb63e831
nadersakr <nader150656@eng.psu.edu.eg>
2024-05-05 00:35:57 +0300

    feat: Update UserViewModel to load user data based on login state and UID

--- social_app/lib/views/screens/profile_screen/profile.dart
+++ social_app/lib/views/screens/profile_screen/profile.dart
@@ -6,157 +6,46 @@ import 'package:provider/provider.dart';
 import 'package:social_app/provider/auth/auth.dart';
 import 'package:social_app/utils/colors.dart';
 import 'package:social_app/views/reusable_components_widgets/plugins/masonry_grid_view.dart';
-// import 'package:social_app/views/screens/Friends/my_friends.dart';
 import 'package:social_app/views/screens/Home/reusable_widgets.dart';
 import 'package:social_app/views/screens/profile_screen/custom_clip_path.dart';
-// import 'package:social_app/views/screens/profile_screen/edit_profile.dart';
 
-// class ProfileScreen extends StatelessWidget {
-//   const ProfileScreen({super.key});
-//   @override
-//   Widget build(BuildContext context) {
-//     AuthController authController =
-//         Provider.of<AuthController>(context, listen: false);
-//     return Scaffold(
-//       // appBar:
-//       //  AppBar(title: const Text('My Profile'), actions: [
-//       //   IconButton(
-//       //       onPressed: () {
-//       //         Navigator.push(context,
-//       //             MaterialPageRoute(builder: (context) =>  const MyFriends()));
-//       //       },
-//       //       icon: const Icon(
-//       //         Icons.people,
-//       //         size: 28,
-//       //       )),
-//       //   const SizedBox(
-//       //     width: 15,
-//       //   )
-//       // ]),
-//       body: SingleChildScrollView(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             Center(
-//               child: CircleAvatar(
-//                 radius: 80,
-//                 backgroundImage: NetworkImage(
-//                   authController.mainUser.avatar!,
-//                 ),
-//               ),
-//             ),
-//             const SizedBox(height: 20),
-//             Center(
-//               child: Text(
-//                 authController.mainUser.userName ?? "User Name",
-//                 style: const TextStyle(
-//                   fontSize: 28,
-//                   fontWeight: FontWeight.bold,
-//                 ),
-//               ),
-//             ),
-//             const SizedBox(height: 10),
-//             Center(
-//               child: Text(
-//                 '${authController.mainUser.bio}',
-//                 style: TextStyle(
-//                   fontSize: 20,
-//                   color: Colors.grey[600],
-//                 ),
-//               ),
-//             ),
-//             const SizedBox(height: 20),
-//             _buildSectionTitle('About Me'),
-//             const SizedBox(height: 10),
-//             Text(
-//               '${authController.mainUser.aboutMe}',
-//               style: TextStyle(
-//                 fontSize: 16,
-//                 color: Colors.grey[800],
-//               ),
-//             ),
-//             const SizedBox(height: 20),
-//             _buildSectionTitle('Contact Information'),
-//             const SizedBox(height: 10),
-//             _buildContactInfoTile(Icons.email, 'Email',
-//                 authController.mainUser.email ?? "user@example.com"),
-//             _buildContactInfoTile(
-//                 Icons.phone, 'Phone', '${authController.mainUser.phone}'),
-//             _buildContactInfoTile(Icons.location_on, 'Address',
-//                 '${authController.mainUser.address}'),
-//             const SizedBox(height: 20),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: FloatingActionButton(
-//           onPressed: () {
-//             Navigator.pushReplacement(
-//                 context,
-//                 MaterialPageRoute(
-//                     builder: (context) => const EditProfileScreen()));
-//           },
-//           child: const Icon(Icons.edit),
-//         ),
-//       ),
-//     );
-//   }
-
-//   Widget _buildSectionTitle(String title) {
-//     return Text(
-//       title,
-//       style: const TextStyle(
-//         fontSize: 20,
-//         fontWeight: FontWeight.bold,
-//       ),
-//     );
-//   }
-
-//   Widget _buildContactInfoTile(IconData icon, String title, String subtitle) {
-//     return ListTile(
-//       leading: Icon(icon),
-//       title: Text(title),
-//       subtitle: Text(subtitle),
-//     );
-//   }
-// }
+import '../../../view_model/profile_viewmodel.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
   @override
   Widget build(BuildContext context) {
+    ProfileViewModel profileViewModel = ProfileViewModel();
     AuthController authController =
         Provider.of<AuthController>(context, listen: false);
     return Scaffold(
       body: Stack(
         children: [
           SizedBox(
-              width: 1.sw,
-              height: 0.4.sh,
+              width: profileViewModel.wholeWidth,
+              height: profileViewModel.profileImageHeight,
               child: SvgPicture.asset(
-                "assets/svg/profile.svg",
+                profileViewModel.profileImage,
                 fit: BoxFit.fill,
               )),
           SizedBox(
-            width: 1.sw,
             child: Column(
               children: [
                 SizedBox(
-                  height: 0.05.sh,
+                  width: profileViewModel.wholeWidth,
+                  height: profileViewModel.heightFromTop,
                 ),
                 Stack(
                   alignment: Alignment.center,
                   children: [
                     SizedBox(
-                      height: 50.h,
+                      height: profileViewModel.heightSpace_50,
                     ),
                     ClipPath(
                       clipper: CustomClipPath(),
                       child: SizedBox(
-                        width: 90.w,
-                        height: 90.w,
+                        width: profileViewModel.squareHeightAndWidth90,
+                        height: profileViewModel.squareHeightAndWidth90,
                         child: Image.network(
                           authController.mainUser.avatar,
                           fit: BoxFit.fill,
@@ -164,57 +53,61 @@ class ProfileScreen extends StatelessWidget {
                       ),
                     ),
                     SizedBox(
-                      width: 100.w,
-                      height: 100.w,
+                      width: profileViewModel.squareHeightAndWidth100,
+                      height: profileViewModel.squareHeightAndWidth100,
                       child: SvgPicture.asset(
-                        "assets/svg/mainShap.svg",
+                        profileViewModel.mainSVG,
                         fit: BoxFit.fill,
                       ),
                     ),
                   ],
                 ),
-                SizedBox(
-                  height: 0.02.sh,
-                ),
-                TextClass.titleText("Nader Sakr", size: 21.sp),
-                SizedBox(
-                  height: 0.01.sh,
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: profileViewModel.paddingHeight,
+                    bottom: profileViewModel.paddingHeightTwo,
+                  ),
+                  child: TextClass.titleText(profileViewModel.fullNameString,
+                      size: 21.sp),
                 ),
                 TextClass(
-                    text: "@${authController.mainUser.userName}", size: 15.sp),
+                    text: "@${profileViewModel.usernameString}", size: 15.sp),
                 SizedBox(
-                  height: 0.05.sh,
+                  height: profileViewModel.heightSpace_50,
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     TextClass.subBodyText(
-                      "Posts",
+                      profileViewModel.postsString,
                       color: AppColors.midGrey,
                     ),
                     TextClass.subBodyText(
-                      "Followers",
+                      profileViewModel.followersString,
                       color: AppColors.midGrey,
                     ),
                     TextClass.subBodyText(
-                      "Follows",
+                      profileViewModel.followsString,
                       color: AppColors.midGrey,
                     ),
                   ],
                 ),
                 SizedBox(
-                  height: 0.008.sh,
+                  height: profileViewModel.verySmallHeight,
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    TextClass.subTitleText("35"),
-                    TextClass.subTitleText("1,506"),
-                    TextClass.subTitleText("128"),
+                    TextClass.subTitleText(
+                        profileViewModel.numberOfPostsString),
+                    TextClass.subTitleText(
+                        profileViewModel.numberOfFollowersString),
+                    TextClass.subTitleText(
+                        profileViewModel.numberOfFollowsString),
                   ],
                 ),
                 SizedBox(
-                  height: 0.02.sh,
+                  height: profileViewModel.paddingHeight,
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -224,19 +117,21 @@ class ProfileScreen extends StatelessWidget {
                       icon: Icon(
                         EneftyIcons.gallery_outline,
                         color: AppColors.darkBlue,
-                        size: 28.sp,
+                        size: profileViewModel.fontSize28,
                       ),
                     ),
                     IconButton(
                       icon: Icon(
                         EneftyIcons.save_2_outline,
-                        size: 28.sp,
+                        size: profileViewModel.fontSize28,
                         color: AppColors.midBlack,
                       ),
                       onPressed: () {},
                     )
                   ],
                 ),
+
+                // TODO Add Dimentions to [profileviewmodel]
                 Padding(
                   padding: EdgeInsets.all(12.w),
                   child: SizedBox(
@@ -249,9 +144,10 @@ class ProfileScreen extends StatelessWidget {
                       itemBuilder: (context, index) {
                         return ClipRRect(
                             borderRadius: BorderRadius.circular(20.sp),
-                            child: Image.network(authController.mainUser.posts![index]
-                                      ['imageUrl']??
-                              "https://img.freepik.com/free-vector/flat-arabic-pattern-background_79603-1826.jpg?size=626&ext=jpg&ga=GA1.1.1039938112.1708868328&semt=sph",
+                            child: Image.network(
+                              authController.mainUser.posts![index]
+                                      ['imageUrl'] ??
+                                  "https://img.freepik.com/free-vector/flat-arabic-pattern-background_79603-1826.jpg?size=626&ext=jpg&ga=GA1.1.1039938112.1708868328&semt=sph",
                               fit: BoxFit.cover,
                             ));
                       },
