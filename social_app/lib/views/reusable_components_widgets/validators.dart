368a8f46eecd9b7840d979abd97a90726d84384e
nadersakr <nader150656@eng.psu.edu.eg>
2024-05-04 05:45:46 +0300

    refactor: Update validators.dart for sign up form validation

--- social_app/lib/views/reusable_components_widgets/validators.dart
+++ social_app/lib/views/reusable_components_widgets/validators.dart
@@ -1,21 +1,65 @@
+import 'package:social_app/view_model/sign_up_viewmodel.dart';
+
 class Tvalidator {
   static String? emailvalidator(value) {
     if (value == null || value.isEmpty) {
-      return 'field email address';
+      return 'Eamil is required';
     } else if (!RegExp(r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$')
         .hasMatch(value)) {
-      return 'pleas enter correct email';
+      return 'Enter a valid email';
     }
     return null;
   }
 
   static String? passwordValidator(value) {
     if (value!.isEmpty) {
-      return 'Fill Password';
+      return 'Password is required';
     } else if (value.length < 8) {
       return 'Password must be more than 8 characters';
     } else {
       return null;
     }
+  }
+
+  static String? confirmPasswordValidator(value) {
+    if (value!.isEmpty) {
+      return 'Confirm Password is required';
+    } else if (value != PasswordHideShow.passwordSignUpController.text) {
+
+      return "Confirm Password doesn't match";
+    } else {
+      return null;
+    }
+  }
+
+  static String? usernameValidator(value) {
+    if (value!.isEmpty) {
+      return SignUpViewModel().usernameRequired;
+    } else if (value.length < 4) {
+      return SignUpViewModel().usernameMoreThan4;
+    }
+    return null;
+  }
+
+  static String? firstNameValidator(value) {
+    if (value == null || value.isEmpty) {
+      return SignUpViewModel().firstnameRequired;
+    } else if (value.length > 9) {
+      return SignUpViewModel().nameLessThan10char;
+    } else if (!RegExp(r'^[a-zA-Z]+$').hasMatch(value)) {
+      return SignUpViewModel().enterValidName;
+    }
+    return null;
+  }
+
+  static String? lastNameValidator(value) {
+    if (value == null || value.isEmpty) {
+      return SignUpViewModel().lastnameRequired;
+    } else if (value.length > 9) {
+      return SignUpViewModel().nameLessThan10char;
+    } else if (!RegExp(r'^[a-zA-Z]+$').hasMatch(value)) {
+      return SignUpViewModel().enterValidName;
+    }
+    return null;
   }
 }
