811a6d31c572abe828f89d7e41cf827a0b842ac4
nadersakr <nader150656@eng.psu.edu.eg>
2024-05-04 23:06:15 +0300

    feat: Add method to load app data in HomeScreenViewModel

---
+++ social_app/lib/view_model/home_viewmodel.dart
@@ -0,0 +1,10 @@
+class HomeScreenViewModel {
+  Future<void> loadAppData()async{
+    // load the user data
+    // load the posts
+    // load the comments
+    // load the likes
+    // load the followers
+    // load the following
+  }
+}
