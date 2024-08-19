af2c678311e7655015dd85e85a1e5a801ca6949f
nadersakr <nader150656@eng.psu.edu.eg>
2024-08-19 20:15:21 +0300

    feat: Update Post model with required fields and constructor

--- social_app/lib/model/post_model.dart
+++ social_app/lib/model/post_model.dart
@@ -1,11 +1,22 @@
-// import 'package:social_app/model/user_model.dart';
+import 'package:social_app/provider/auth/auth.dart';
 
-// class Post {
-//   String? postId = "nnnnnnnnn";
-//   MainUser? owner;
-//   List<MainUser> likes = [];
-//   Map<MainUser, String> comments = {};
-//   String? imageurl =
-//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYscfUBUbqwGd_DHVhG-ZjCOD7MUpxp4uhNe7toUg4ug&s";
-//   List<MainUser> shares = [];
-// }
+class Post {
+  int postId;
+  String owner;
+  String time;
+  List<String> likes = [];
+  Map<String, String> comments = {};
+  String? ContentImageurl;
+  List<String> shares = [];
+  Post(
+      {required this.time,
+      required this.postId,
+      required this.ContentImageurl,
+      required this.owner});
+
+ factory Post.OwnPost(String postTime,int postId,String imageUrl) => Post(
+      time: postTime,
+      postId: postId,
+      ContentImageurl: imageUrl,
+      owner: AuthController.mainUser.userUID??"");
+}
