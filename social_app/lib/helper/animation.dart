8c7bbd72a85562cc0b34bebf0ec3b1644a57ebe4
nadersakr <nader150656@eng.psu.edu.eg>
2024-08-19 20:15:07 +0300

    refactor: Rename AnimationLoder to AnimationLoader

--- social_app/lib/helper/animation.dart
+++ social_app/lib/helper/animation.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-class AnimationLoder extends StatelessWidget {
+class AnimationLoader extends StatelessWidget {
   final String animated;
   final double width;
  
-  const AnimationLoder(
+  const AnimationLoader(
       {super.key, required this.animated, required this.width});
 
   @override
